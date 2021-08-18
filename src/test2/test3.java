44426143e9e0d3a795583d8330977964bb0ac22d
moiradenisez <mdzarzuela@student.hau.edu.ph>
2021-08-18 15:44:22 +0800

    Initial commit

---
+++ src/test2/test3.java
@@ -0,0 +1,14 @@
+package test2;
+
+public class test3 {
+    public String getTest4() {
+        return test4;
+    }
+
+    public void setTest4(String test4) {
+        this.test4 = test4;
+    }
+
+    public String test4;
+
+}
